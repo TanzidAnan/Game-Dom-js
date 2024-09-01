a26bbc1c667a42248b42494868ec163f99601570
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-02 00:22:34 +0600

    palyer two button click

--- js.js
+++ js.js
@@ -4,6 +4,12 @@ let buttonOne =document.querySelector('.buttonOne');
 let error = document.querySelector('.error')
 
 
+let headingTwo =document.querySelector('.headingtwo')
+let inputTwo =document.querySelector('.inputtwo')
+let buttonTwo =document.querySelector('.buttontwo');
+let error1 = document.querySelector('.error1')
+
+
 
 
 
@@ -20,6 +26,26 @@ buttonOne.addEventListener('click',function(){
         error.innerHTML='please Enter a number 1 to 10';
     }
     else{
-        console.log('Ok')
+        headingTwo.style.display ='block';
+        inputTwo.style.display ='inline-block';
+        buttonTwo.style.display ='inline-block';
+        error1.style.display ='block';
+
+
+        headingOne.style.display ='none';
+        inputOne.style.display ='none';
+        buttonOne.style.display ='none';
+        error.style.display ='none';
+    }
+});
+buttonTwo.addEventListener('click',function(){
+    if(!inputTwo.value){
+        error.innerHTML='Enter sonething';
+    }
+    else if(isNaN(inputTwo.value)){
+        error.innerHTML="Enter Number:";
+
+    }else if(!(inputTwo.value <=10 && inputTwo.value >0)){
+        error.innerHTML='please Enter a number 1 to 10';
     }
 })
