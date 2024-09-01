774cfcef0d9c66d3dcf26fceb394e4176cabca88
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-01 21:19:19 +0600

    Enter number

--- js.js
+++ js.js
@@ -3,5 +3,10 @@ let inputOne =document.querySelector('.inputOne')
 let buttonOne =document.querySelector('.buttonOne')
 
 buttonOne.addEventListener('click',function(){
-    console.log(5454)
+    if(!inputOne.value){
+        console.log('Enter sonething')
+    }
+    else if(isNaN(inputOne.value)){
+        console.log("Enter Number")
+    }
 })
