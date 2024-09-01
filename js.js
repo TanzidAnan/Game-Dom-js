c842eac0c8341939dba6ad1a684452045cdd0c66
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-01 21:50:53 +0600

    enter your Numer

--- js.js
+++ js.js
@@ -6,12 +6,15 @@ let error = document.querySelector('.error')
 
 
 
+
+
+
 buttonOne.addEventListener('click',function(){
     if(!inputOne.value){
         error.innerHTML='Enter sonething';
     }
     else if(isNaN(inputOne.value)){
-        error.innerHTML="Enter Number";
+        error.innerHTML="Enter Number:";
 
     }else if(!(inputOne.value <=10 && inputOne.value >0)){
         error.innerHTML='please Enter a number 1 to 10';
