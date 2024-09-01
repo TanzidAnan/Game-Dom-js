05181fa90c8f6c50339398cef792becc9de43657
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-01 21:49:46 +0600

    html page error massage

--- js.js
+++ js.js
@@ -1,15 +1,20 @@
 let headingOne =document.querySelector('.headingOne')
 let inputOne =document.querySelector('.inputOne')
-let buttonOne =document.querySelector('.buttonOne')
+let buttonOne =document.querySelector('.buttonOne');
+let error = document.querySelector('.error')
+
+
+
 
 buttonOne.addEventListener('click',function(){
     if(!inputOne.value){
-        console.log('Enter sonething')
+        error.innerHTML='Enter sonething';
     }
     else if(isNaN(inputOne.value)){
-        console.log("Enter Number")
+        error.innerHTML="Enter Number";
+
     }else if(!(inputOne.value <=10 && inputOne.value >0)){
-        console.log('please Enter a number 1 to 10')
+        error.innerHTML='please Enter a number 1 to 10';
     }
     else{
         console.log('Ok')
