c0a7f0ae5710dec34aaa46b3612581675d76d1f0
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-01 21:12:25 +0600

    button click

---
+++ js.js
@@ -0,0 +1,7 @@
+let headingOne =document.querySelector('.headingOne')
+let inputOne =document.querySelector('.inputOne')
+let buttonOne =document.querySelector('.buttonOne')
+
+buttonOne.addEventListener('click',function(){
+    console.log(5454)
+})
