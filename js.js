9f1bfe63ec671311bb8ce615bb5dc571293d98b5
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-02 10:34:29 +0600

    count done

--- js.js
+++ js.js
@@ -1,15 +1,15 @@
-let headingOne =document.querySelector('.headingOne')
-let inputOne =document.querySelector('.inputOne')
-let buttonOne =document.querySelector('.buttonOne');
+let headingOne = document.querySelector('.headingOne')
+let inputOne = document.querySelector('.inputOne')
+let buttonOne = document.querySelector('.buttonOne');
 let error = document.querySelector('.error')
 
 
-let headingTwo =document.querySelector('.headingtwo')
-let inputTwo =document.querySelector('.inputtwo')
-let buttonTwo =document.querySelector('.buttontwo');
+let headingTwo = document.querySelector('.headingtwo')
+let inputTwo = document.querySelector('.inputtwo')
+let buttonTwo = document.querySelector('.buttontwo');
 let error1 = document.querySelector('.error1')
 let chance = document.querySelector('.chance');
-let count =5
+let count = 5;
 
 
 
@@ -17,43 +17,53 @@ let count =5
 
 
 
-buttonOne.addEventListener('click',function(){
-    if(!inputOne.value){
-        error.innerHTML='Enter sonething';
+buttonOne.addEventListener('click', function () {
+    if (!inputOne.value) {
+        error.innerHTML = 'Enter sonething';
     }
-    else if(isNaN(inputOne.value)){
-        error.innerHTML="Enter Number:";
+    else if (isNaN(inputOne.value)) {
+        error.innerHTML = "Enter Number:";
 
-    }else if(!(inputOne.value <=10 && inputOne.value >0)){
-        error.innerHTML='please Enter a number 1 to 10';
+    } else if (!(inputOne.value <= 10 && inputOne.value > 0)) {
+        error.innerHTML = 'please Enter a number 1 to 10';
     }
-    else{
-        headingTwo.style.display ='block';
-        inputTwo.style.display ='inline-block';
-        buttonTwo.style.display ='inline-block';
-        error1.style.display ='block';
-        chance.innerHTML =`Chance : ${chance}`
+    else {
+        headingTwo.style.display = 'block';
+        inputTwo.style.display = 'inline-block';
+        buttonTwo.style.display = 'inline-block';
+        error1.style.display = 'block';
+        chance.innerHTML = `Chance : ${count}`
 
 
-        headingOne.style.display ='none';
-        inputOne.style.display ='none';
-        buttonOne.style.display ='none';
-        error.style.display ='none';
+        headingOne.style.display = 'none';
+        inputOne.style.display = 'none';
+        buttonOne.style.display = 'none';
+        error.style.display = 'none';
     }
 });
-buttonTwo.addEventListener('click',function(){
-    if(!inputTwo.value){
-        error.innerHTML='Enter sonething';
+buttonTwo.addEventListener('click', function () {
+    if (!inputTwo.value) {
+        error.innerHTML = 'Enter sonething';
     }
-    else if(isNaN(inputTwo.value)){
-        error.innerHTML="Enter Number:";
+    else if (isNaN(inputTwo.value)) {
+        error.innerHTML = "Enter Number:";
 
-    }else if(!(inputTwo.value <=10 && inputTwo.value >0)){
-        error.innerHTML='please Enter a number 1 to 10';
+    } else if (!(inputTwo.value <= 10 && inputTwo.value > 0)) {
+        error.innerHTML = 'please Enter a number 1 to 10';
     }
-    else{
-        if(inputOne.value ==inputTwo.value){
-            console.log(654654)
+    else {
+        if (count > 1) {
+            count--;
+            chance.innerHTML = `Chance : ${count}`;
+            if (inputOne.value == inputTwo.value) {
+                console.log('plear two winer')
+            }
         }
+        else{
+            count =0;
+            chance.innerHTML = `Chance : ${count}`;
+            console.log('plear one winer')
+        }
+
     }
 })
